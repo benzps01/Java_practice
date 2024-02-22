5180d2bb240a945c3bb0295957edee08289daf1b
benzps01 <benzps01@gmail.com>
2024-02-22 23:57:15 +0530

    testing data types in Java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String name = "Benson";
+        int number = 25;
+        System.out.println("Hello " + name + ". Your age is " + number);
     }
 }
