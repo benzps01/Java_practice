5718d578f51546914b762079d0369070385832aa
benzps01 <benzps01@gmail.com>
2024-03-07 22:05:35 +0530

    first program

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
-        String name = "Benson";
-        int number = 25;
-        System.out.println("Hello " + name + ". Your age is " + number);
+class Main {
+    public static void main(String args[]) {
+        // First program
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
     }
 }
