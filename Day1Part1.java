abc4b00c614ae55fef710603380a687dd87cd372
benzps01 <benzps01@gmail.com>
2024-03-07 22:29:03 +0530

    Changed Main function to Day1Part1 and added scanner function part

---
+++ Day1Part1.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Day1Part1 {
+    public static void main(String args[]) {
+        // First program
+        /*
+         * System.out.println("*");
+         * System.out.println("**");
+         * System.out.println("***");
+         * System.out.println("****");
+         */
+
+        // variables
+        /*
+         * int a = 10;
+         * int b = 25;
+         * //int sum = a + b;
+         * // System.out.println(sum);
+         * 
+         * int mul = a * b;
+         * System.out.println(mul);
+         */
+
+        // Input
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
